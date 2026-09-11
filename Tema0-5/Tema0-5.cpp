189ebda95fe2e06f65004c8b1b1abf16160aad24
ossilv01 <ossilv01@ucm.es>
2026-09-11 13:07:01 +0200

    Tema0-5

---
+++ Tema0-5/Tema0-5.cpp
@@ -0,0 +1,64 @@
+// Oscar Silva Urbina
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+// función que resuelve el problema
+int resolver(int datos) {
+    //multiplicar por 2 y separar con espacios los elementos de la lista
+    datos = datos * 2; 
+    //std::cout << datos << " ";
+
+    return datos;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int nelem; 
+    std::cin >> nelem;
+
+   for (int j = 0; j < nelem; j++) {
+      int num;
+      std::cin >> num;
+      int sol = resolver(num);
+      std::cout << sol <<" ";
+   }
+   std::cout << std::endl;
+
+
+    //---
+    //int datos; 
+    //int sol = resolver(datos);
+
+    // escribir sol
+
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+    std::cout << std::endl;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}

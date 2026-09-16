f8e930a3b4505e92595897642af4e4c163b89402
ossilv01 <ossilv01@ucm.es>
2026-09-16 13:19:42 +0200

    Tema1-02 timelimit, plantillas y datos en 1-03 y 1-04
    Mucho for anidado, ver forma de optimizar

---
+++ Tema1-02E/Tema1-02E.cpp
@@ -0,0 +1,83 @@
+// Oscar Silva Urbina 
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+// función que resuelve el problema
+//pico
+int resolver(vector <int> v, int&npicos, int&nvalles) {
+    
+    for (int i = 0; i < v.size()-2; i++) {
+        int izq = v[i];
+        int medio = 0;
+        if (i + 1 < v.size()) {
+            medio = v[i + 1];
+        }
+        int dcha = 0;
+        if (i + 2 < v.size()) {
+            dcha = v[i + 2];
+        }
+
+        if (izq < medio && medio > dcha) npicos++;
+        if (izq > medio && medio < dcha) nvalles++;
+    }
+    return 0;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+
+void resuelveCaso() {
+    // leer los datos de la entrada y creamos lista con huecos x
+    int ntemperaturas;
+    cin >> ntemperaturas;
+    vector <int> lista(ntemperaturas);
+
+    int npicos = 0;
+    int nvalles = 0; 
+
+    //metemos los valores exactos a la lista
+    for (int i = 0; i < lista.size(); i++) {
+        int elem;
+        cin >> elem; 
+        lista[i] = elem;
+    }
+    
+    resolver(lista, npicos, nvalles);
+    cout << npicos << " " << nvalles;
+
+    //for (int e : lista) {
+    //    cout << e << " ";
+    //}
+    cout << std::endl;
+    // 
+    //TipoSolucion sol = resolver(datos);
+    // escribir sol
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}

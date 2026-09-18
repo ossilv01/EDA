2e1c296f18d17ca2e1d471799fd7c918b890f00c
ossilv01 <ossilv01@ucm.es>
2026-09-18 10:57:34 +0200

    Update Tema1-03.cpp

--- Tema1-03/Tema1-03.cpp
+++ Tema1-03/Tema1-03.cpp
@@ -1,28 +1,79 @@
 // Oscar Silva Urbina
+// Coste O(n) 
 //numerosos casos
+//Observaciones: A lo mejor se puede meter todo en el mismo for inicial
+//al menos para crear los 2 vectores y a lo mejor no es necesario
+// el primer vector creado si no mas bien ir cogiendo cin >> 
+// y procesarlo al momento
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 // función que resuelve el problema
-int resolver(const vector <int> &v, int &p) {
-    vector <int> menores;
-    vector <int> mayores; 
-
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] <= v[p]) menores.push_back(v[i]);
+string resolver(const vector <int> &v, int &p) {
+   /* vector <int> menores;
+    vector <int> mayores; */
+    //Creo 2 sub-vectores distintos, uno conteniendo los elementos 
+    //hasta p, y otro conteniendo los elementos de la derecha de p
+    /*for (int i = 0; i < v.size(); i++) {
+        if (i <= p) menores.push_back(v[i]);
         else mayores.push_back(v[i]);
+    }*/
+
+    //casos especiales donde vector menores o mayores vacio
+    if ( p <0 || p>=v.size() - 1) {
+        return "SI";
     }
     
-    for (int e : menores) std::cout << e << " ";
-    cout << endl;
-    for (int a : mayores) std::cout << a << " ";
-        
-    return 0; 
+    int mayorMenores = v[0];
+    for (int i = 0; i <= p; i++) {
+        if (v[i] > mayorMenores) mayorMenores = v[i];
+    }
+    int menorMayores = v[p+1];
+
+    for (int i = p + 2; i < v.size(); i++) {
+        if (v[i] < menorMayores) menorMayores = v[i];
+    }
+
+    //casos especiales
+    //vector menores o mayores vacio
+    /*if (menores.size() == 0 || mayores.size() == 0) {
+        a = "SI";
+        return a;
+    }*/
+    ////vector mayores vacio
+    //if (mayores.size() ==0) {
+    //    a = "SI";
+    //    return a;
+    //}
+
+   
+
+    //buscamos el elemento mas grande de vector menores
+    //for (int num : menores) {
+    //    if (num > mayorMenores)
+    //        mayorMenores = num;
+    //}
+
+    ////buscamos el eleemento mas pequeño de vector mayores
+    //for (int num : mayores) {
+    //    if (num < menorMayores)
+    //        menorMayores = num;
+    //}
+    //
+    //for (int e : menores) std::cout << e << " ";
+    //cout << endl;
+    //for (int a : mayores) std::cout << a << " ";
+    //
+    //cout << endl;
+    //Damos veredicto
+    if (mayorMenores < menorMayores) return "SI";
+    else return "NO";
 
 }
 
@@ -43,9 +94,9 @@ void resuelveCaso() {
         vector[i] = elem;
     }
 
-    int sol = resolver(vector, p);
+    string sol = resolver(vector, p);
     // escribir sol
-    cout << endl; 
+    cout << sol << "\n";
 
 
 }

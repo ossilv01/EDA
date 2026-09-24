e96b918802910ce3e7c0e0c10cc58e243112d0ee
ossilv01 <ossilv01@ucm.es>
2026-09-24 12:54:45 +0200

    Falta hacerlo TOTALMENTE recursivo y mucho mejor

--- Tema2-02/Tema2-02.cpp
+++ Tema2-02/Tema2-02.cpp
@@ -1,23 +1,70 @@
 // Oscar Silva Urbina 
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <cmath>
+using namespace std; 
+//metodo recursivo 1 (complementario) 
+int recomplementario(int dato) {
+    int nexpo =0; 
+    int aux = 0;
+    int numfinal = 0; 
+   
+    while (dato/10 != 0) {
+        aux = dato % 10;
+        aux = 9 - aux;
+        aux = aux * pow(10, nexpo);
+        nexpo++;
+        numfinal += aux;
+        dato = dato / 10;
+    }
+    if (dato <= 9 || dato >= 0) {
+        aux = 9 - dato;
+        aux = aux * pow(10, nexpo);
+        numfinal += aux;
+    }
+    return numfinal;
+}
+//metodo recursivo 2 (inverso) 
+int reinverso(int dato) {
+    int aux1 = dato; 
+    int nexpo = 0; 
+    int numfinal = 0;
+    while (aux1 / 10 != 0) {
+        aux1 = aux1/10; 
+        nexpo++;
+    }
 
+    while (dato / 10 != 0) {
+        aux1 = dato % 10;
+        aux1 = aux1 * pow(10, nexpo);
+        nexpo--;
+        numfinal += aux1;
+        dato = dato / 10;
+    }
+    if (dato <= 9 || dato >= 0) {
+        numfinal += dato;
+    }
+    return numfinal;
+}
 
 // función que resuelve el problema
-TipoSolucion resolver(TipoDatos datos) {
-
-
+int resolver(int &datos) {
+    int a = recomplementario(datos);
+    int b = reinverso(a);
+    cout << a << " " << b << endl; 
+    return 0; 
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 void resuelveCaso() {
     // leer los datos de la entrada
-
-
-    TipoSolucion sol = resolver(datos);
+    int num;
+    cin >> num; 
+    //cout << num << endl; 
+    resolver(num);
+    //int sol = resolver(lol);
     // escribir sol
 
 

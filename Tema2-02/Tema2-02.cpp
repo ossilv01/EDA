a7b44e1a8ba22aedc5c0afb25dd25ae8e2a94d4c
Your Name <ossilv01@ucm.es>
2026-09-25 13:12:39 +0200

    Update Tema2-02.cpp

--- Tema2-02/Tema2-02.cpp
+++ Tema2-02/Tema2-02.cpp
@@ -2,57 +2,92 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <cmath>
 using namespace std; 
-//metodo recursivo 1 (complementario) 
-int recomplementario(int dato) {
-    int nexpo =0; 
-    int aux = 0;
-    int numfinal = 0; 
-   
-    while (dato/10 != 0) {
-        aux = dato % 10;
-        aux = 9 - aux;
-        aux = aux * pow(10, nexpo);
-        nexpo++;
-        numfinal += aux;
-        dato = dato / 10;
-    }
-    if (dato <= 9 || dato >= 0) {
-        aux = 9 - dato;
-        aux = aux * pow(10, nexpo);
-        numfinal += aux;
-    }
-    return numfinal;
-}
-//metodo recursivo 2 (inverso) 
-int reinverso(int dato) {
-    int aux1 = dato; 
-    int nexpo = 0; 
-    int numfinal = 0;
-    while (aux1 / 10 != 0) {
-        aux1 = aux1/10; 
-        nexpo++;
-    }
 
-    while (dato / 10 != 0) {
-        aux1 = dato % 10;
-        aux1 = aux1 * pow(10, nexpo);
-        nexpo--;
-        numfinal += aux1;
-        dato = dato / 10;
-    }
-    if (dato <= 9 || dato >= 0) {
-        numfinal += dato;
-    }
-    return numfinal;
+//metodo complementario
+//int recomplementario(int dato) {
+//    int pos = 1; 
+//    int numfinal = 0; 
+//   
+//    while (dato/10 != 0) {
+//        int aux = dato % 10;
+//        aux = 9 - aux;
+//        numfinal += aux * pos;
+//        pos = pos * 10;
+//        dato = dato / 10;
+//    }
+//
+//
+//   /* while (dato/10 != 0) {
+//        int aux = dato % 10;
+//        aux = 9 - aux;
+//        aux = aux * pow(10, nexpo);
+//        nexpo++;
+//        numfinal += aux;
+//        dato = dato / 10;
+//    }
+//    if (dato <= 9 || dato >= 0) {
+//        int aux = 9 - dato;
+//        aux = aux * pow(10, nexpo);
+//        numfinal += aux;
+//    }*/
+//    return numfinal;
+//}
+
+int RecComplementario (int dato){
+    //Condición base para detenerse:
+    if (dato>=0 && dato<=9) {
+        return 9-dato;
+     }
+     int aux = dato % 10;
+     aux = 9 - aux;
+     return (RecComplementario(dato/10)*10)+aux;   
+     
 }
+
+// int CoplementarioREC(int datos) {
+//     //Rango limitado por enunciado
+//    if (datos <0 || datos > 1000000000) return datos; 
+//    return RecComplementario(datos); 
+//}
+
+
+int InversoREC(int dato, int numfinal) {
+    if (dato >= 0 && dato <= 9) {
+        return numfinal * 10 + dato; 
+    }
+    else {
+        int aux1 = dato % 10; 
+        numfinal = numfinal * 10 + aux1;
+        dato = dato / 10; 
+        return InversoREC(dato, numfinal);
+    }
+}
+
+int RecInverso(int dato) {
+    return InversoREC(dato, 0);
+}
+
+//metodo (inverso) 
+//int reinverso(int dato) {
+//    int numfinal = 0;
+//    int aux =0; 
+//    while (dato != 0) {
+//        aux = dato % 10;
+//        numfinal = (numfinal * 10) + aux;
+//        dato = dato / 10;
+//    }
+//
+//    return numfinal;
+//}
 
 // función que resuelve el problema
 int resolver(int &datos) {
-    int a = recomplementario(datos);
-    int b = reinverso(a);
-    cout << a << " " << b << endl; 
+    int c = RecComplementario(datos);
+    int d = RecInverso(c);
+    //int a = recomplementario(datos);
+    //int b = reinverso(a);
+    cout << c << " " << d << " " << /*"Original:" << a << " " << b <<*/ endl;
     return 0; 
 }
 

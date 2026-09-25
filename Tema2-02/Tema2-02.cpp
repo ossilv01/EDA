8090470f20dbd09a133be62b38e4675acd577d88
Your Name <ossilv01@ucm.es>
2026-09-25 14:36:18 +0200

    Terminado 2-02
    El segundo metodo tenia que coger de dato directo del  "cin >> dato" no tenia que procesar el resultado del primer metodo pues se perdia informacion valiosa como es el del 0 a la izquierda
    
    Entrada:
    90
    Salida:
    9 90
    
    Leer mejor para la próxima vez

--- Tema2-02/Tema2-02.cpp
+++ Tema2-02/Tema2-02.cpp
@@ -4,35 +4,6 @@
 #include <fstream>
 using namespace std; 
 
-//metodo complementario
-//int recomplementario(int dato) {
-//    int pos = 1; 
-//    int numfinal = 0; 
-//   
-//    while (dato/10 != 0) {
-//        int aux = dato % 10;
-//        aux = 9 - aux;
-//        numfinal += aux * pos;
-//        pos = pos * 10;
-//        dato = dato / 10;
-//    }
-//
-//
-//   /* while (dato/10 != 0) {
-//        int aux = dato % 10;
-//        aux = 9 - aux;
-//        aux = aux * pow(10, nexpo);
-//        nexpo++;
-//        numfinal += aux;
-//        dato = dato / 10;
-//    }
-//    if (dato <= 9 || dato >= 0) {
-//        int aux = 9 - dato;
-//        aux = aux * pow(10, nexpo);
-//        numfinal += aux;
-//    }*/
-//    return numfinal;
-//}
 
 int RecComplementario (int dato){
     //Condición base para detenerse:
@@ -41,54 +12,22 @@ int RecComplementario (int dato){
      }
      int aux = dato % 10;
      aux = 9 - aux;
-     return (RecComplementario(dato/10)*10)+aux;   
+     dato = dato / 10;
+     return (RecComplementario(dato)*10)+aux;   
      
 }
-
-// int CoplementarioREC(int datos) {
-//     //Rango limitado por enunciado
-//    if (datos <0 || datos > 1000000000) return datos; 
-//    return RecComplementario(datos); 
-//}
 
 
 int InversoREC(int dato, int numfinal) {
     if (dato >= 0 && dato <= 9) {
-        return numfinal * 10 + dato; 
+        return numfinal * 10 + (9-dato); 
     }
     else {
         int aux1 = dato % 10; 
-        numfinal = numfinal * 10 + aux1;
+        numfinal = numfinal * 10 + (9-aux1);
         dato = dato / 10; 
         return InversoREC(dato, numfinal);
     }
-}
-
-int RecInverso(int dato) {
-    return InversoREC(dato, 0);
-}
-
-//metodo (inverso) 
-//int reinverso(int dato) {
-//    int numfinal = 0;
-//    int aux =0; 
-//    while (dato != 0) {
-//        aux = dato % 10;
-//        numfinal = (numfinal * 10) + aux;
-//        dato = dato / 10;
-//    }
-//
-//    return numfinal;
-//}
-
-// función que resuelve el problema
-int resolver(int &datos) {
-    int c = RecComplementario(datos);
-    int d = RecInverso(c);
-    //int a = recomplementario(datos);
-    //int b = reinverso(a);
-    cout << c << " " << d << " " << /*"Original:" << a << " " << b <<*/ endl;
-    return 0; 
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
@@ -97,10 +36,11 @@ void resuelveCaso() {
     // leer los datos de la entrada
     int num;
     cin >> num; 
-    //cout << num << endl; 
-    resolver(num);
-    //int sol = resolver(lol);
+
+    int c = RecComplementario(num);
+    int d = InversoREC(num, 0);
     // escribir sol
+    cout << c << " " << d << endl; 
 
 
 }
